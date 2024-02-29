58d80077276ce7bb5a963ae56d6611c08c999879
gustavo litter <gustavolitter@gmail.com>
2024-02-29 09:49:54 -0300

    Adjusted the database connection, and the output messages

--- src/utils/connection.rs
+++
@@ -1,8 +0,0 @@
-use mysql_async::{Opts, Pool, Conn};
-
-pub async fn connection() -> (Pool, Conn, String) {
-  let db_host = "mysql://root:sa1234@localhost:3306";
-  let connect = Pool::new(Opts::from_url(&db_host).unwrap());
-  let conn = connect.get_conn().await.unwrap();
-  (connect, conn, db_host.to_string())
-}
